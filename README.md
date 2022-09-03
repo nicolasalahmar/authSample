01f99b735182982b7be7b355bd25db4ed42ea35c
nicolasalahmar <nicolasalahmar@gmail.com>
2022-09-03 13:13:24 +0300

    minor changes

--- README.md
+++ README.md
@@ -1,7 +1,8 @@
-i am running two servers for the refresh token because php artisan serve is not a real server
-however this required me to change the port if the passport
+i deleted running two servers and now i created a middleware to help me to edit the request going to login and refresh token and it is called oauth.
 
 
 
 my method is using password grant client but it is apparently discouraged and especially for 2 factor authentication in the future so i should
 refactor my code to use authorization code grant apparently in the future
+
+check why the scheduler in kernel is not working i had it set up to delete old tokens but it is not working
--- app/Http/Controllers/PassportAuth/LoginController.php
+++ app/Http/Controllers/PassportAuth/LoginController.php
@@ -2,77 +2,20 @@
 
 namespace App\Http\Controllers\PassportAuth;
 
+use Laravel\Passport\TokenRepository;
+use Laravel\Passport\RefreshTokenRepository;
 use App\Http\Controllers\Controller;
-use App\Models\User;
-use Laravel\Passport\Client as OClient;
-use App\Providers\RouteServiceProvider;
-use Illuminate\Foundation\Auth\AuthenticatesUsers;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Http;
 
 class LoginController extends Controller
 {
-    use AuthenticatesUsers;
-
-    protected $redirectTo = RouteServiceProvider::HOME;
-
-    public function __construct()
-    {
-        $this->middleware('guest')->except('logout');
-    }
-
-    protected function sendLoginResponse(Request $request)
-    {
-        $this->clearLoginAttempts($request);
-
-        if ($response = $this->authenticated($request, $this->guard()->user())) {
-            return $response;
-        }
-
-        return $request->wantsJson()
-            ? new JsonResponse([], 204)
-            : redirect()->intended($this->redirectPath());
-    }
-
-    protected function sendFailedLoginResponse(Request $request)
-    {
-        return User::where('email',$request->email)->exists()?
-            response()->json(['message'=>'false password.']):
-            response()->json(['message'=>'false credentials.']);
-    }
-
-    public function login(Request $request)
-    {
-        $this->validateLogin($request);
-
-        // If the class is using the ThrottlesLogins trait, we can automatically throttle
-        // the login attempts for this application. We'll key this by the username and
-        // the IP address of the client making these requests into this application.
-        if (method_exists($this, 'hasTooManyLoginAttempts') &&
-            $this->hasTooManyLoginAttempts($request)) {
-            $this->fireLockoutEvent($request);
-
-            return $this->sendLockoutResponse($request);
-        }
-
-        if ($this->attemptLogin($request)) {
-            return $this->sendLoginResponse($request);
-        }
-
-        // If the login attempt was unsuccessful we will increment the number of attempts
-        // to login and redirect the user back to the login form. Of course, when this
-        // user surpasses their maximum number of attempts they will get locked out.
-        $this->incrementLoginAttempts($request);
-
-        return $this->sendFailedLoginResponse($request);
-    }
-
     public function logout(Request $request)
     {
-        $token = $request->user()->token();
+        $tokenId = $request->user()->token()->id;
 
-        $token->revoke();
+        app(TokenRepository::class)->revokeAccessToken($tokenId);
+        app(RefreshTokenRepository::class)->revokeRefreshTokensByAccessTokenId($tokenId);
 
         if ($response = $this->loggedOut($request)) {
             return $response;
